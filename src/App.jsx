e9e6cbc9173111c2d28d98d2727c1e0a54e56826
BrianSayen <blu3fire89@gmail.com>
2022-08-16 10:34:07 +0800

    update: remove react-router-dom pckge

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css'
 import Hotel from './pages/hotel'
-import Onboarding from './pages/onboarding'
-import { Routes, Route, Link } from "react-router-dom";
 function App() {
 
   return (
-    <Routes>
-      <Route exact path="/react-websites/hotel" element={<Hotel />} />
-      <Route exact path="/react-websites/onboarding" element={<Onboarding/>} />
-    </Routes>
+    <>
+      <Hotel />
+    </>
   )
 }
 
